499e2a069f5d45e794ec3f24d78e5ed3ea04188f
YasuY1 <lovesoulmusic@icloud.com>
2022-05-29 23:35:48 +0900

    style初期値修正

--- test.js
+++ test.js
@@ -1,12 +1,17 @@
 const allArea = document.querySelectorAll('.area');
+
 // ボックスの成形
 for(let i=0; i<allArea.length; i++){
     const area = allArea[i];
     const areaArrow = area.querySelectorAll('.arrow');
 
-    for(let n = 0; n < areaArrow.length; n++){
-        const arrows = area.getElementsByClassName('arrow')[n];
-        arrows.style.width = '200px';
-        arrows.style.height = '50px';
+    i % 2 === 0 ? styleShaper('100px'): styleShaper('200px');
+
+    function styleShaper(widthValue){
+        for(let n = 0; n < areaArrow.length; n++){
+            const arrows = area.getElementsByClassName('arrow')[n];
+            arrows.style.height = '50px';
+            arrows.style.width = widthValue;
+        }
     }
 }
